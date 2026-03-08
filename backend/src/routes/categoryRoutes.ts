610bf854c5ccae82ed5d0940ff1a13d7d1e7b257
Evans Njenga Matindi <150904148+Tindi0110@users.noreply.github.com>
2026-03-08 16:44:58 +0300

    chore: final cleanup of Node.js artifacts, leave only Django backend

--- backend/src/routes/categoryRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getCategories, createCategory } from '../controllers/categoryController';
-
-const router = Router();
-
-router.get('/', getCategories);
-router.post('/', createCategory);
-
-export default router;
